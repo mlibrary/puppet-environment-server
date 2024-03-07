ba7b662ab9f7ae54ccd6aa1d0341a35b8d0d9567
Anthony Thomas <antmoth@umich.edu>
2024-03-07 14:33:55 -0500

    fix 'main' require

--- web.rb
+++ web.rb
@@ -1,5 +1,16 @@
-require 'sinatra'
+require "sinatra"
+require_relative "lib/main"
 
-get '/' do
+get "/" do
   return "hello yes this is reposync dot container dot jpg"
+end
+
+get "/deploy/:ref" do
+  sync = Reposync.new(params['ref'])
+  sync.deploy!
+end
+
+get "/update/:ref" do
+  sync = Reposync.new(params['ref'])
+  sync.update_libraries!
 end
